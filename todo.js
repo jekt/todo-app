51727048d64dea483fd7dc23087e25bc43567361
Jekt <jekt.tl@gmail.com>
2015-01-28 00:32:44 +0100

    Plug to redis

--- todo.js
+++ todo.js
@@ -2,190 +2,108 @@
 
 var program = require('commander'),
 	redis = require('redis'),
-	redisClient = redis.createClient(6379, 'localhost'),
-	todoList = [];
+	redisClient = redis.createClient(6379, 'localhost');
 
-redisClient.on("error", function (err) {
-    console.log(err);
+redisClient.on("error", function (err){
+	console.log(err);
 });
 
+var listTasks = function () {
+		redisClient.lrange('todoList', 0, -1, function (err, res){
+			console.log('\nTO DO:');
+			if (res && res.length > 0) {
+				res.reverse();
+				res.forEach(function(val, i){
+					console.log('\t' + (i+1) + '. ' + val);
+				});
+			} else {
+				console.log('\tNothing yet...');
+			}
+		});
+		redisClient.lrange('doneList', 0, -1, function (err, res){
+			console.log('\nDONE:');
+			if (res && res.length > 0) {
+				res.reverse();
+				res.forEach(function(val, i){
+					console.log('\t' + (i+1) + '. ' + val);
+				});
+			} else {
+				console.log('\tNothing yet...');
+			}
+		});
+	},
 
-
-/** 
- * Get task index from redis
- * @param {string} name The name of the task to get
- * @return {int} The index of the task in the todoList array (-1 if it not exists)
- */
-var getTaskIndex = function (name) {
-		switch (typeof(name)){
-			case 'string':
-				try {
-					for (var i=0, x=todoList.length; i<x; i++) {
-	    				if (todoList[i].name === name) {
-	    					return i;
-	    				}
-					}
-				} catch (err) {
-					throw err;
-				}
-				break;
-
-			case 'undefined':
-			default:
-				console.log('Please specify the name of the task');
-				return -1;
+	rmTask = function (task){
+		if (task && (typeof(task) === 'string')){
+			redisClient.lrem('todoList', 1, task, function (err, res){
+				console.log(task + ' has been removed!');
+				listTasks();
+			});
+		} else {
+			console.log('Please specify the exact name of the task');
 		}
 	},
 
-/** 
- * Remove tasks from redis
- * @param {string} name The name of the task to remove
- * @return {object} The object containing the wanted task
- */
-	rmTask = function (name){
-		try {
-			var task = todoList.splice(getTaskIndex(name), 1);
-			console(task + 'deleted');
-		} catch (err) {
-			throw err;
+	storeTask = function (task){
+		if (task && (typeof(task) === 'string')){
+			redisClient.lpush('todoList', task, function(err, res){
+				console.log('New task added: "' + task + '"');
+				listTasks();
+			});
+		} else {
+			console.log('Please specify a valid name for your new task');
 		}
 	},
 
-/** 
- * Store tasks in redis
- * @param {object} task The object containing the task data
- * @param {string} name The name of the task to replace (optional)
- * @return {array} The array containing all the tasks
- */
-	storeTask = function (name, options){
-		switch (typeof(options.name)){
-			case 'string':
-				var index = getTaskIndex(name);
-				if (index > -1) {
-					console.log(rmTask(todoList[index]));
+	doTask = function (task){
+		if (task && (typeof(task) === 'string')){
+			redisClient.lrem('todoList', 1, task, function(err, res){
+				if (res > 0) {
+					console.log('nested');
+					redisClient.lpush('doneList', task, function(err, res){
+						console.log(task + ' > done, Bravo!');
+						listTasks();
+					});
 				} else {
-					console.log("Cannot update this task, it doesn't exist");
+					console.log('Task doesn\'t exist!');
 				}
-				break;
-
-			case 'undefined':
-			default:
-				var name = options.name;
-				if (name) {
-					var date = ['today', 'tomorrow', 'upcoming', 'someday'].indexOf(options.date) > -1 ? options.date : 'upcoming',
-						priority = ['blocker', 'critical', 'major', 'minor', 'trivial'].indexOf(options.priority) > -1 ? options.priority : 'minor',
-						status = 'todo';
-					console.log(
-						todoList.push(
-							JSON.stringify({
-								'name': name,
-								'date': date,
-								'priority': priority,
-								'status': status
-							})
-						)
-					);
-				} else {
-					console.log('Please specify a name for your new task');
-				}
+			});
+		} else {
+			console.log('Please specify the exact name of the task');
 		}
 	},
 
-/** 
- * List tasks stored in redis
- * @return {array} The todoList array
- */
-	listTasks = function () {
-		console.log(todoList);
+	flushTasks = function (){
+		redisClient.flushall(function (){
+			console.log('Done list flushed!');
+			listTasks();
+		});
 	};
 
-/** 
- * List tasks in the todo list
- * @param {string} date List the taks for the date
- * @options {string} -i --include include List option: all, todo, doing, or done
- */
+
 program
-	.command('list [date]')
+	.command('list')
 	.alias('li')
-	.option('-i, --include [include]', 'list option: all, todo, doing, or done')
 	.description('list tasks in the todo list')
 	.action(listTasks);
-	/*function(date, options){
-		date = ['today', 'tomorrow', 'upcoming', 'someday'].indexOf(date) > -1 ? date : 'today';
-		var include = ['all', 'todo', 'doing', 'done'].indexOf(options.include) > -1 ? options.include : 'all';
-		//console.log('List ' + include + ' tasks in the todo list for date: ' + date);
-		console.log(listTasks());
-	});*/
 
-/** 
- * Add a new task in the todo list
- * @param {string} name Name of the task
- * @option {date} -d --date date Set a due date for the task
- * @option {string} -p --priority priority Set a priority for the task
- */
 program
-	.command('add [name]')
-	.alias('new')
-	.option('-d, --date [date]', 'set a due date for the task')
-	.option('-p, --priority [priority]', 'set a priority for the task')
-	.description('list tasks in the todo list')
-	.action(storeTask);
-
-/** 
- * Edit an existing task
- * @param {string} name The name of the task to edit
- * @option {string} -n --name name Replace the existing name by this one
- * @option {string} -d --date date Replace the existing date by this one
- * @option {string} -p --priority priority Replace the existing priority by this one
- */
-program
-	.command('edit [name]')
-	.alias('update')
-	.alias('ed')
-	.option('-n, --name [name]', 'edit the name of the task')
-	.option('-d, --date [date]', 'edit the due date of the task')
-	.option('-p, --priority [priority]', 'edit the priority of the task')
-	.description('list tasks in the todo list')
-	.action(storeTask);
-
-/** 
- * Mark a task as "done"
- * @param {string} name The name of the task to set as "done"
- */
-program
-	.command('done [name]')
+	.command('done [task]')
 	.alias('ok')
 	.description('mark a task as "done"')
-	.action(function(name, options){
-		console.log(storeTask);
-	});
+	.action(doTask);
 
-/** 
- * Mark a task as "doing"
- * @param {string} name The name of the task to set as "doing"
- */
 program
-	.command('doing [name]')
-	.alias('work')
-	.alias('do')
-	.description('mark a task as "doing"')
-	.action(function(name, options){
-		console.log(storeTask);
-	});
+	.command('flush')
+	.alias('fl')
+	.description('flush the whole list')
+	.action(flushTasks);
 
-/** 
- * Delete a task
- * @param {string} name The name of the task to delete
- */
 program
-	.command('delete [name]')
-	.alias('del')
-	.alias('remove')
-	.alias('rm')
-	.description('delete a task')
-	.action(function(name, options){
-		console.log(rmTask);
-	});
+	.command('add [task]')
+	.alias('new')
+	.description('add task in the todo list')
+	.action(storeTask);
 
 program
 	.version('0.0.1')
@@ -193,4 +111,6 @@ program
 
 if (!process.argv.slice(2).length) {
     program.outputHelp();
-  }
+}
+listTasks();
+redisClient.quit();
